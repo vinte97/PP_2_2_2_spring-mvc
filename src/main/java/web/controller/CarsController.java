6effaca66550ec0ffce786a13c605dffc2d9b76b
yura-pvl@mail.ru <yura-pvl@mail.ru>
2022-11-07 14:56:04 +0600

    Completed task with КОСТЫЛИ)

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,15 +1,31 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import web.models.Car;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarsService;
 
 @Controller
 public class CarsController {
-    @GetMapping(value = "/cars")
-    public String getCars(@ModelAttribute("car") Car car) {
 
+    private CarsService carsService;
+
+    @Autowired
+    public CarsController(CarsService carsService) {
+        this.carsService = carsService;
     }
+
+    @GetMapping(value = "/cars")
+    public String getCars(@RequestParam(defaultValue = "5", value = "count") int count,
+                          Model model) {
+        if (count >= 3) {
+            model.addAttribute("cars", carsService.getCarList());
+        } else {
+            model.addAttribute("cars", carsService.getCarList(count));
+        }
+        return "cars";
+    }
+
 }
--- src/main/java/web/service/CarsService.java
+++ src/main/java/web/service/CarsService.java
@@ -1,14 +1,19 @@
 package web.service;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Component
 public class CarsService {
 
     private List<Car> carList;
+
     {
         carList = new ArrayList<>();
         carList.add(new Car("BMW M3", 777, 2007));
@@ -18,8 +23,12 @@ public class CarsService {
         carList.add(new Car("Toyota mark II", 222, 1994));
     }
 
+    public List<Car> getCarList(int count) {
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+
     public List<Car> getCarList() {
-        return carList.stream().toArray()
+        return carList.stream().collect(Collectors.toList());
     }
 
 }
